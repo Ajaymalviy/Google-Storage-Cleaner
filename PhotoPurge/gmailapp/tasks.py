6047830d72e26f06bd4281a74d14e5f865b00519
t <ajeymalviya143@gmail.com>
2025-09-05 01:29:07 +0530

    arrange the modules of import and comments"

--- PhotoPurge/gmailapp/tasks.py
+++ PhotoPurge/gmailapp/tasks.py
@@ -62,7 +62,6 @@ def delete_emails_task(self, user_id, email, category):
 			results = service.users().messages().list(
 				userId="me", q=query, pageToken=page_token
 			).execute()
-
 			messages_list = results.get("messages", [])
 
 			if not messages_list:
@@ -84,7 +83,6 @@ def delete_emails_task(self, user_id, email, category):
 			page_token = results.get("nextPageToken")
 			if not page_token:
 				break
-
 		result_message = f"Deleted {deleted_count} emails in category {category}"
 
 
@@ -107,21 +105,17 @@ def delete_emails_task(self, user_id, email, category):
 @shared_task(bind=True)
 def recover_emails_task(self, user_id, email):
 	try:
-
 		task_status, created = RecoverStatus.objects.get_or_create(
 			task_id=self.request.id,
 			user_id=user_id
 		)
-
 		task_status.status = "IN_PROGRESS"
 		task_status.save()
-
 		creds = retrieve_credentials_for_user(user_id)
 		if creds.expiry and isinstance(creds.expiry, str):
 			creds.expiry = datetime.strptime(creds.expiry.replace(' ', 'T'), '%Y-%m-%dT%H:%M:%S.%f')
 
 		service = build("gmail", "v1", credentials=creds)
-
 		recover_count = 0
 		next_page_token = None
 		messages_list = []  
@@ -132,9 +126,7 @@ def recover_emails_task(self, user_id, email):
 					creds.refresh(Request())
 					task_status.result = "Refreshing token"
 					task_status.save()
-
 					creds = retrieve_credentials_for_user(user_id)
-
 					social_account = SocialAccount.objects.get(user_id=user_id, provider='google')
 					social_token = SocialToken.objects.get(account=social_account)
 					social_token.token = creds.token
@@ -151,12 +143,9 @@ def recover_emails_task(self, user_id, email):
 				labelIds=["TRASH"],
 				pageToken=next_page_token
 			).execute()
-
 			next_page_token = results.get("nextPageToken")
-
 			if not next_page_token:
 				break
-
 			messages_list.extend(results.get("messages", []))
 
 		if not messages_list:
@@ -164,7 +153,6 @@ def recover_emails_task(self, user_id, email):
 			task_status.result = "No emails found in Trash."    
 			task_status.save()
 			return task_status.result
-
 
 		for message in messages_list:
 			msg_id = message['id']
@@ -174,11 +162,9 @@ def recover_emails_task(self, user_id, email):
 				id=msg_id,
 				body={"removeLabelIds": ["TRASH"]}
 			).execute()
-
 			recover_count += 1  
 
 		result_message = f"Recovered {recover_count} emails."
-
 		task_status.status = "SUCCESS"
 		task_status.result = result_message
 		task_status.recover_count = recover_count  
@@ -186,12 +172,10 @@ def recover_emails_task(self, user_id, email):
 		message = f"Your {task_status.recover_count} emails have been recovered successfully! {task_status.result}. Thanks for choosing CODEMOS"
 		email = EmailMessage('Emails deleted', message, to=[email])
 		email.send()
-
 		return result_message, recover_count
 
 	except Exception as e:
 		task_status.status = "FAILURE"
 		task_status.result = f"An error occurred: {e}"
 		task_status.save()
-
 		return task_status.result
