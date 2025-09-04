8b07d8a89c14acd26c1ee8e2522db3798dafd7b3
t <ajeymalviya143@gmail.com>
2025-09-04 23:47:57 +0530

    added new changes into task file and make it in correct formate

--- PhotoPurge/gmailapp/tasks.py
+++ PhotoPurge/gmailapp/tasks.py
@@ -1,20 +1,22 @@
 from celery import shared_task
 from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from allauth.socialaccount.models import SocialAccount, SocialToken
+from django.core.mail import EmailMessage 
+from datetime import datetime, timezone
+from .models import TaskStatus, RecoverStatus
+from .auth import check_token_validity, refresh_google_token  
 from .utils import retrieve_credentials_for_user
 import logging 
-from .models import TaskStatus, RecoverStatus
 import requests
-from django.core.mail import EmailMessage 
-from .auth import check_token_validity, refresh_google_token  
 import time
-from datetime import datetime, timezone
-from google.auth.transport.requests import Request
-from allauth.socialaccount.models import SocialAccount
 #with bind=True we can use self in the function
 @shared_task(bind=True)
 def delete_emails_task(self, user_id, email, category):
 	try:
-		# task_status variable creates a new object of TaskStatus class or if it already existing it fetches its data by task_id and user_id and created is boolean variable which returns True or False for created or not created
+		# task_status variable creates a new object of TaskStatus class or if it already existing
+		# it fetches its data by task_id and user_id and created is boolean variable
+		# which returns True or False for created or not created
 		task_status, created = TaskStatus.objects.get_or_create(
 			task_id=self.request.id,
 			user_id=user_id
@@ -22,8 +24,7 @@ def delete_emails_task(self, user_id, email, category):
 	except Exception as e:
 		return f"mera exception hai {e}"
 
-	try:
-		
+	try:		
 		task_status.status = "IN_PROGRESS"
 		task_status.save()
 		
@@ -34,33 +35,34 @@ def delete_emails_task(self, user_id, email, category):
 
 		# builds service for gmail api
 		service = build("gmail", "v1", credentials=creds)
-		
+
+		# query to search emails in the category selected by user
 		query = f"category:{category.split('_')[1].lower()}"
 		deleted_count = 0
 		page_token = None
-		
+
+		# loop to fetch all emails in the selected category
 		while True:
 			try:
 				if not check_token_validity(creds.token):
 					creds.refresh(Request())
 					task_status.result = "Refreshing token"
 					task_status.save()
-
 					creds = retrieve_credentials_for_user(user_id)
-
 					social_account = SocialAccount.objects.get(user_id=user_id, provider='google')
 					social_token = SocialToken.objects.get(account=social_account)
 					social_token.token = creds.token
 					social_token.expires_at = creds.expiry
 					social_token.save()
 					time.sleep(15)
-
-					
+				# print('Token is valid') 	
 			except Exception as e:
-				return f"kjsgbksjbgksb {e}"
+				return f"error is : {e}"
+			
 			results = service.users().messages().list(
 				userId="me", q=query, pageToken=page_token
 			).execute()
+
 			messages_list = results.get("messages", [])
 
 			if not messages_list:
@@ -93,16 +95,12 @@ def delete_emails_task(self, user_id, email, category):
 		message = f"Your {task_status.deleted_count} emails have been deleted successfully! {task_status.result}. Thanks for choosing CODEMOS"
 		email = EmailMessage('Emails deleted', message, to=[email])
 		email.send()
-
-
 		return result_message, messages_list
 
 	except Exception as e:
 		task_status.status = "FAILURE"
 		task_status.result = f"An error occurred: {e}"
 		task_status.save()
-
-
 		return task_status.result
 
 
